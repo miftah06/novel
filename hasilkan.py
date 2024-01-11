33ba574263fb7423a609995ec262158520bf617c
miftah06 <izharuddinmiftah@gmail.com>
2024-01-11 08:52:24 +0800

    update

--- hasilkan.py
+++ hasilkan.py
@@ -4,7 +4,7 @@ import random
 
 def generate_object_names(keywords_file, num_objects=1000):
     # Membaca kata kunci dari file
-    with open(keywords_file, 'r') as file:
+    with open(keywords_file, 'r', encoding='utf-8') as file:
         keywords = file.read().splitlines()
 
     # Memastikan jumlah kata kunci cukup untuk menghasilkan objek
@@ -22,6 +22,17 @@ def generate_object_names(keywords_file, num_objects=1000):
     df = pd.DataFrame(data)
 
     return df
+
+# Contoh penggunaan
+keywords_file = 'keyword.txt'  # Ganti dengan file yang berisi kata kunci
+num_objects_to_generate = 10000  # Ganti dengan jumlah objek yang ingin dihasilkan
+generated_objects = generate_object_names(keywords_file, num_objects_to_generate)
+
+# Menyimpan DataFrame ke file CSV
+generated_objects.to_csv('katakunci.txt', index=False)
+
+print(f"{num_objects_to_generate} Nama objek telah disimpan ke dalam katakunci.txt")
+
 
 # Contoh penggunaan
 keywords_file = 'keyword.txt'  # Ganti dengan file yang berisi kata kunci
